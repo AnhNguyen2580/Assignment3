f3c3b266b7223b4cfe42900298e6f13415eba2c0
AnhNguyen2580 <84497927+AnhNguyen2580@users.noreply.github.com>
2021-10-27 23:24:29 +0700

    API

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ app.use("/users", usersRoutes);
 app.get("/", (req, res) => res.send("Welcome to the Users API!"));
 app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
 
-app.listen(process.env.PORT || port, () =>console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(process.env.PORT || port, () =>console.log(`Server running on port: http://localhost:${port}`));
